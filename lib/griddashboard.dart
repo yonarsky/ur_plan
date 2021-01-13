1ddf49d4d7f355441e55bcdbacec4f08f7777abc
yonarsky <adrianjonarski@gmail.com>
2021-01-13 23:18:33 +0100

    grid dasboard

---
+++ lib/griddashboard.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GridDashboard extends StatelessWidget {
+  Items item1 = new Items(
+    title: "Kolegium Nauk Humanistycznych",
+    event: "H",
+  );
+
+  Items item2 = new Items(
+    title: "Kolegium Nauk Medycznych",
+    event: "M",
+  );
+  Items item3 = new Items(
+    title: "Kolegium Nauk Przyrodniczych",
+    event: "P",
+  );
+  Items item4 = new Items(
+    title: "Kolegium Nauk Społecznych",
+    event: "S",
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    List<Items> myList = [item1, item2, item3, item4];
+    //var color = 0xff453658;
+    return Flexible(
+      child: GridView.count(
+          childAspectRatio: 1.0,
+          padding: EdgeInsets.only(left: 16, right: 16),
+          crossAxisCount: 2,
+          crossAxisSpacing: 18,
+          mainAxisSpacing: 18,
+          children: myList.map((data) {
+            return Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue[900], borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    data.event,
+                    style: GoogleFonts.ubuntu(
+                        textStyle: TextStyle(
+                            color: Colors.white24,
+                            fontSize: 48,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    data.title,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                ],
+              ),
+            );
+          }).toList()),
+    );
+  }
+}
+
+class Items {
+  String title;
+  String event;
+  Items({this.title, this.event});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'griddashboard.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Plan zajęć UR',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.light,
+        primarySwatch: MaterialColor(
+            Colors.blue.shade900.value,
+            {
+              50: Colors.blue.shade50,
+              100: Colors.blue.shade100,
+              200: Colors.blue.shade200,
+              300: Colors.blue.shade300,
+              400: Colors.blue.shade400,
+              500: Colors.blue.shade500,
+              600: Colors.blue.shade600,
+              700: Colors.blue.shade700,
+              800: Colors.blue.shade800,
+              900: Colors.blue.shade900
+            }
+        ),
+        accentColor: Colors.blue[900],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: MaterialColor(
+            Colors.blue.shade900.value,
+            {
+              50: Colors.blue.shade50,
+              100: Colors.blue.shade100,
+              200: Colors.blue.shade200,
+              300: Colors.blue.shade300,
+              400: Colors.blue.shade400,
+              500: Colors.blue.shade500,
+              600: Colors.blue.shade600,
+              700: Colors.blue.shade700,
+              800: Colors.blue.shade800,
+              900: Colors.blue.shade900
+            }
+        ),
+        accentColor: Colors.blue[900],
+      ),
+      home: MyHomePage(title: 'Plan zajęć UR'),
     );
   }
 }
@@ -34,14 +57,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +65,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  /*void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Plan zajęć UR'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 40,
+          ),
+          GridDashboard()
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
